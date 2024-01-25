0ad96bf8895b516a52c57a097009b599e2fcade1
Eduardo Junior <juniorcose@gmail.com>
2024-01-25 19:38:44 -0300

    fix vercel6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import CustomCursor from "@/components/CustomCursor";
-import EarthSection from "@/components/EarthSection";
-import ExpandedCards from "@/components/ExpandedCards";
-import GallerySection from "@/components/GallerySection";
+
 import Hero from "@/components/Hero";
-import SecondSection from "@/components/SecondSection";
-import TextScale from "@/components/TextScale";
-import TextSticky from "@/components/TextSticky";
-import WhiteSceen from "@/components/WhiteSceen";
+
 import Footer from "@/components/footer/Footer";
 
 export default function Home() {
@@ -16,13 +11,6 @@ export default function Home() {
       <Hero />
       <div className="flex flex-col w-full relative ">
         <div className="absolute overflow-hidden z-10 top-0 left-0 w-full h-32 bg-gradient-to-b from-[#2E253A] to-transparent"></div>
-        <SecondSection />
-        <TextScale />
-        <WhiteSceen />
-        <GallerySection />
-        <TextSticky />
-        <ExpandedCards />
-        <EarthSection />
       </div>
       <Footer />
     </main>
