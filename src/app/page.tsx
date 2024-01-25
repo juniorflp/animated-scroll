bb0d061ef279109944f8e94cf66438eb71d4ce33
Eduardo Junior <juniorcose@gmail.com>
2024-01-25 19:42:12 -0300

    fix vercel7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
+import ContainerLenis from "@/components/ContainerLenis";
 import CustomCursor from "@/components/CustomCursor";
 
 import Hero from "@/components/Hero";
+import SecondSection from "@/components/SecondSection";
 
 import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
     <main className="relative flex flex-col w-full">
-      <CustomCursor />
-      <Hero />
-      <div className="flex flex-col w-full relative ">
-        <div className="absolute overflow-hidden z-10 top-0 left-0 w-full h-32 bg-gradient-to-b from-[#2E253A] to-transparent"></div>
-      </div>
-      <Footer />
+      <ContainerLenis>
+        <CustomCursor />
+        <Hero />
+        <div className="flex flex-col w-full relative ">
+          <div className="absolute overflow-hidden z-10 top-0 left-0 w-full h-32 bg-gradient-to-b from-[#2E253A] to-transparent"></div>
+          <SecondSection />
+        </div>
+        <Footer />
+      </ContainerLenis>
     </main>
   );
 }
